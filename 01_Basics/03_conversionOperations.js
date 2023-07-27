5c5773b173662f675ecf6c79b9bbbd58c7586908
Priyansu Sahoo <priyansusahoo1@gmail.com>
2023-07-27 07:39:52 +0000

    added: Various Operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -32,5 +32,53 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/**
+ * OPERATIONS
+ */
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// // Important Concept:
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// // ---
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); // 1
+/**
+ * Will be 1 because by adding '+' symbol at start 
+ * it will be converted to numberic value i.e 1.
+ */
+// console.log(+""); 
+/**
+ * 0 {because empty string is false and by adding '+' 
+ * it will be converted to numberic value}. 
+ */
+  let num1, num2, num3
+
+  num1 = num2 = num3 = 2 + 2
+
+  let gameCounter = 100;
+//   gameCounter++;
+  ++gameCounter;
+  console.log(gameCounter);
